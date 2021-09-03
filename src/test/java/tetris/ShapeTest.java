3a118e65f3d28ce8979d9ea3523d6eb9d4b70f27
Kerkko Pelttari <kerk.pelt@gmail.com>
2021-09-04 00:23:27 +0300

    Remove unnecessary tests

---
+++ src/test/java/tetris/ShapeTest.java
@@ -0,0 +1,21 @@
+package tetris;
+
+import tetris.Shape;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.api.FxToolkit;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeoutException;
+
+public class ShapeTest {
+    @Before
+    public void setUp() throws TimeoutException {
+    }
+    
+    @Test
+    public void basicShapeTest() {
+		var shape = new Shape(1);
+		shape.
+    }
+}
